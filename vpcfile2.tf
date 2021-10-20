0809941bff1870044fa84af0390d94a7ac294233
renuka <rsppatil0421@gmail.com>
2021-10-20 19:32:53 +0530

    vpc 2nd file commited

---
+++ vpcfile2.tf
@@ -0,0 +1,60 @@
+# Create VPC
+resource "aws_vpc" "vpc" {
+  cidr_block              = "${var.vpc-cidr}"
+  instance_tenancy        = "default"
+  enable_dns_hostnames    = true
+
+  tags      = {
+    Name    = "Test VPC"
+  }
+}
+
+
+
+# Create Internet Gateway and Attach it to VPC
+
+resource "aws_internet_gateway" "internet-gateway" {
+  vpc_id    = aws_vpc.vpc.id
+
+  tags      = {
+    Name    = "Test IGW"
+  }
+}
+
+
+
+# Create Public Subnet 
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "${var.public-subnet-1-cidr}"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags      = {
+    Name    = "Public Subnet 1"
+  }
+}
+
+
+# Create Route Table and Add Public Route
+resource "aws_route_table" "public-route-table" {
+  vpc_id       = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-gateway.id
+  }
+
+  tags       = {
+    Name     = "Public Route Table"
+  }
+}
+
+# Associate Public Subnet 1 to "Public Route Table"
+# terraform aws associate subnet with route table
+resource "aws_route_table_association" "public-subnet-1-route-table-association" {
+  subnet_id           = aws_subnet.public-subnet-1.id
+  route_table_id      = aws_route_table.public-route-table.id
+}
+
+
